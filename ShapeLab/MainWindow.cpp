84ba9808eff545ae3a2bc5f13963dacefe0e02aa
Xiangjia CHEN <Singachen@cuhk.edu.hk>
2020-06-01 15:40:50 +0800

    0601-1

--- ShapeLab/MainWindow.cpp
+++ ShapeLab/MainWindow.cpp
@@ -14,6 +14,8 @@
 #include <QMessageBox>
 #include <QScreen>
 
+#include <QChart>
+
 #include <fstream>
 
 #include "../GLKLib/GLKCameraTool.h"
@@ -769,23 +771,23 @@ void MainWindow::QMeshPatchRangeActive()
 }
 void MainWindow::test_in_mainwindow()
 {
-	QChart* chart = new QChart();
+	//QChart* chart = new QChart();
 
-	// 构建折线系列对象
-	QLineSeries* series = new QLineSeries();
-	for (quint32 i = 0; i < 100; i++)
-	{
-		// 参数 x 为循环自增变量 i，参数 y 为正弦函数Y值
-		series->append(i, sin(static_cast<double>(0.6f * i)));
-	}
+	//// 构建折线系列对象
+	//QLineSeries* series = new QLineSeries();
+	//for (quint32 i = 0; i < 100; i++)
+	//{
+	//	// 参数 x 为循环自增变量 i，参数 y 为正弦函数Y值
+	//	series->append(i, sin(static_cast<double>(0.6f * i)));
+	//}
 
-	// 将系列添加到图表
-	chart->addSeries(series);
-	// 基于已添加到图表的 series 来创建默认的坐标轴
-	chart->createDefaultAxes();
+	//// 将系列添加到图表
+	//chart->addSeries(series);
+	//// 基于已添加到图表的 series 来创建默认的坐标轴
+	//chart->createDefaultAxes();
 
-	// 将图表绑定到视图
-	ui->widget->setChart(chart);
+	//// 将图表绑定到视图
+	//ui->widget->setChart(chart);
 
 
 
